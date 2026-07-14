0ff1d0419b60e16dcb600e395db456428f3a4cd3
francishillary69 <francishillary69@gmail.com>
2026-07-14 10:24:03 +0300

    Apply dark background theme with blue and green accents to all pages

--- salamasalesltd/asgi.py
+++ salamasalesltd/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MtaaniStore project.
+ASGI config for Salama Sales Ltd project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MtaaniStore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salamasalesltd.settings')
 
 application = get_asgi_application()
